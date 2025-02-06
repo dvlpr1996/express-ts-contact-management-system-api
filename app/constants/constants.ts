a3e74571bfe9284811f1c7663f891b822f57a9b5
dvlpr1996 <nimajahanbakhshian@gmail.com>
2025-02-07 02:25:57 +0330

    fix constant type issues

--- app/constants/constants.ts
+++ app/constants/constants.ts
@@ -1,4 +1,4 @@
-import { RoleEnum } from "../types/types";
+import { RoleEnum } from '../types/types';
 
 export const Role = {
   ADMIN: RoleEnum.ADMIN,
@@ -7,7 +7,8 @@ export const Role = {
 
 export const API_ROUTE_VERSION: string = '/api/v1';
 
-export const JWT_EXPIRES_IN_TIME: string = process.env.EXPIRES_IN!;
+export const JWT_EXPIRES_IN_TIME: string | number = process.env.EXPIRES_IN!;
 export const PASSPORT_JWT_SECRET_KEY: string = process.env.JWT_SECRET!;
+export const PASSPORT_JWT_TOKEN_NAME: string = 'token';
 export const PASSPORT_JWT_SIGNED_COOKIE_NAME: string = process.env.PASSPORT_JWT_COOKIE_NAME!;
 export const COOKIE_PARSER_SECRET_KEY: string = process.env.COOKIE_PARSER_SECRET_KEY!;
